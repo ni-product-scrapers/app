40750a67e3c4df6a66fe7a2d6ee439f8bddab175
Renato Longobardi <renato.longobardi@native-instruments.de>
2019-12-17 15:49:26 +0100

    improving shoprow and product-card

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -5,14 +5,14 @@ import Link from 'next/link';
 
 const ProductCard = ({ title, sku, image, attributes }) => {
 
-    const description = attributes && attributes.find( ({key}) => key === 'description' ).value;
+    // const description = attributes && attributes.find( ({key}) => key === 'description' ).value;
 
     return(
       <Link href={`/products/${sku}`}>
         <Card width={256} css={css`cursor: pointer; margin: 0 auto;`}>
           <Image src={image} />
           <Heading fontSize={2} mb={2}>{title}</Heading>
-          <Text fontSize={1}>{description || ''}</Text>
+          <Text fontSize={1}>{}</Text>
         </Card>
       </Link>
     )
--- components/ShopRow.js
+++ components/ShopRow.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
-import { Box, Text, Button } from 'rebass';
+import { Box, Text, Button, Heading } from 'rebass';
 
 const ShopRow = ({ shop, title, price, currency, shop_logo, product_url, main }) => {
 
@@ -12,7 +12,7 @@ const ShopRow = ({ shop, title, price, currency, shop_logo, product_url, main })
       sx={{ padding: 3, marginBottom: 3 }}
       css={css`
         display: grid;
-        grid-template-columns: 1fr 1fr 1fr auto;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
         row-gap: 1em;
         border: 2px solid #eee;
         border-radius: 6px;
@@ -21,23 +21,23 @@ const ShopRow = ({ shop, title, price, currency, shop_logo, product_url, main })
       `}
     >
       <Box>
-        <img
+        {shop_logo ? <img
           css={css`
             width: 100px;
           `}
-          src={shop_logo}
-        />
+          src={shop_logo || ''}
+        /> : <Heading fontSize={2} css={css`color:#009688;`}>{shop}</Heading>}
+
       </Box>
       <Box>
         <Text fontWeight="bold" px={1} css={css`color: ${isPriceMinor ? '#e91e63' : '#4caf50'};`}>
           {price || 'ND'} {currency || 'ND'}
           {isPriceMinor ? ' 😱 ' : ' 👍' }
-
         </Text>
       </Box>
       <Box pr={2}>{title || 'ND'}</Box>
       <Box>
-        <Button>
+        <Button width="100%">
           <a
             target="_blank"
             css={css`
